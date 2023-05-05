41cace283af838eadff53f56ea5a7b4221b57ea5
Hurri-01 <hureya.arebi@gmail.com>
2023-05-05 10:49:06 -0700

    Print the first 50 Fibonacci numbers, separated by a comma and a space

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,9 +14,9 @@ int main(void)
 
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li %li ", a, b);
+		printf("%li, %li", a, b);
 
-		if (counter != (countto / 2))
+		if (counter != countto / 2)
 		       printif(", ");
 
 		a += b;
