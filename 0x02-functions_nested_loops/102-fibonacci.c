f00ba9ae81b237e3fab638da0b7cf1cb5a0b37dd
Hurri-01 <hureya.arebi@gmail.com>
2023-05-05 08:32:05 -0700

    rint first 50 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,23 +6,24 @@
  * Return: 0
  */
 
-int main(void)
-{
-	int counter;
-	int countto = 50;
-	long a = 1;
-	long b = 2;
-
-	for (counter = 1; counter <= (countto / 2); counter++)
-	{
-		printf("%li %li ", a, b);
-		a += b;
-		b += a;
-	}
-	if (countto % 2 == 1)
-		printf("%li", a);
-
-	printf("\n");
-
-	return (0);
+int main() {
+    int i, n = 50;
+    int fib[n];
+    
+    fib[0] = 1;
+    fib[1] = 2;
+    
+    for (i = 2; i < n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    
+    for (i = 0; i < n; i++) {
+        printf("%d", fib[i]);
+        if (i < n-1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+    
+    return 0;
 }
