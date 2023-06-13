8da1fec980b0173ecc7fe730e190272b977735b4
Hurri-01 <hureya.arebi.com>
2023-06-12 19:46:33 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,18 +1,12 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-
 #include <unistd.h>
-
-
 #include <stdio.h>
-
-
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
